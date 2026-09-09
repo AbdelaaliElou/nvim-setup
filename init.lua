346e312b88808b5cd9b139c855f1c7d83043bea5
abdelaali EL OUASSOULI <abdelaali@local>
2026-09-09 13:10:32 +0100

    require web devicons

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ require("config.filetypes")
 
 -- Plugins
 require("plugins.mason-nvim")
+require("plugins.webdevicons-nvim")
 require("plugins.roslyn-nvim")
 require("plugins.nvim-treesitter")
 require("plugins.telescope-nvim")
