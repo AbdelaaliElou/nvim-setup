f41e68fc3c7305884985e3a69059c6dd60625914
abdelaali EL OUASSOULI <abdelaali@local>
2026-09-09 15:27:58 +0100

    set line number, indent width, and tabstop

--- lua/config/global.lua
+++ lua/config/global.lua
@@ -1,5 +1,8 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.cmd("set number")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=2")
 
 vim.pack.add({
     { src = "https://github.com/rebelot/kanagawa.nvim" }
